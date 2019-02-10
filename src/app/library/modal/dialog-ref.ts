ed743d203efa71fdfb57240e4eb2f037936ef4ff
alfonsserra <alfonsoserra@gmail.com>
2019-02-10 17:09:52 +0100

    Add Escape and back mouse listener

--- src/app/library/modal/dialog-ref.ts
+++ src/app/library/modal/dialog-ref.ts
@@ -3,9 +3,21 @@ import { Observable, Subject } from 'rxjs';
 
 export class DialogRef<T> {
 
+  private static readonly ESCAPE_KEY = 'Escape';
+
   private subject: Subject<any> = new Subject<any>();
 
   constructor(private overlayRef: OverlayRef, public context?: T) {
+    overlayRef.backdropClick()
+      .subscribe(
+        () => this.close());
+
+    overlayRef.keydownEvents()
+      .subscribe((k) => {
+        if (k.code === DialogRef.ESCAPE_KEY) {
+          this.close();
+        }
+      });
   }
 
   public close(value?: any): void {
